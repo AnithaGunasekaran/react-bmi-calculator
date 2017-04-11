17b74f9d24f5575e844cbaca57789c40c81c84ab
Anithaa <anithag@mercerbell.com.au>
2017-04-11 14:41:00 +1000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+     super(props);
+     this.state = { name: 'Guest', weight: 90, height: 180, bmi: 27, message: '', optimalweight: '', time: new Date().toLocaleTimeString() };
+    
+     this.submitMe = this.submitMe.bind(this);
+     this.heightchange = this.heightchange.bind(this);
+     this.weightchange = this.weightchange.bind(this);
+     this.change = this.change.bind(this);  
+     this.ticker = this.ticker.bind(this);  
+    
+  }
+
+
+  heightchange(e){
+    this.setState({height: e.target.value});
+    e.preventDefault();
+  }
+
+   weightchange(e){
+    this.setState({weight: e.target.value});
+
+    e.preventDefault();
+  }
+
+  submitMe(e) {
+     e.preventDefault();
+     var heightSquared = (this.state.height/100  * this.state.height/100);
+     var bmi = this.state.weight / heightSquared;
+     var low = Math.round(18.5 * heightSquared);                                                         
+     var high = Math.round(24.99 * heightSquared);    
+     var message = "";
+     if( bmi >= 18.5  && bmi <= 24.99 ){
+        message = "You are in a healthy weight range";
+     }
+     else if(bmi >= 25 && bmi <= 29.9){
+       message = "You are overweight";
+     }
+     else if(bmi >= 30){
+        message ="You are obese";
+     }
+     else if(bmi < 18.5){
+       message = "You are under weight";
+     }
+     this.setState({message: message});  
+     this.setState({optimalweight: "Your suggested weight range is between "+low+ " - "+high});    
+     this.setState({bmi: Math.round(bmi * 100) / 100});   
+  }
+
+  ticker() {
+    this.setState({time: new Date().toLocaleTimeString()})
+    
+  }
+ 
+  componentDidMount(){
+    setInterval(this.ticker, 60000);
+  }
+
+ 
+  change(e){
+    e.preventDefault();
+    console.log(e.target);
+    this.setState({name: e.target.value});
+  }
+
+ 
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>BMI Calculator</h2>
+        </div>
+          <form onSubmit={this.submitMe}>
+            <label>
+              Please enter your name
+            </label>
+            <input type="text" name="name" value={this.state.name} onChange={this.change}   />
+             <label>
+             Enter your height in cm: 
+            </label>
+            <input type="text" name="height" value={this.state.height} onChange={this.heightchange}   />
+             <label>
+             Enter your weight in kg : 
+            </label>
+            <input type="text" name="weight" value={this.state.weight} onChange={this.weightchange}    />
+            <label>{this.state.checked} Hello {this.state.name}, How are you my friend? It's currently  {this.state.time} where you are living. Your BMI is {this.state.bmi} </label>
+              <label>{this.state.message}</label>
+              <label>{this.state.optimalweight}</label>
+             
+            <input type="submit" value="Submit"/>
+          </form>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
